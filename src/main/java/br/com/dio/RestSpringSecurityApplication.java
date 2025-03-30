c14cac4dad7d282bf6441d8e90e2edae6b414186
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2025-03-30 16:48:55 -0300

    basic auth

--- src/main/java/br/com/dio/RestSpringSecurityApplication.java
+++ src/main/java/br/com/dio/RestSpringSecurityApplication.java
@@ -2,14 +2,12 @@ package br.com.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
-public class RestSpringApplication {
+public class RestSpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestSpringApplication.class, args);
+		SpringApplication.run(RestSpringSecurityApplication.class, args);
 	}
 
 }
--- src/test/java/br/com/dio/RestSpringSecurityApplicationTests.java
+++ src/test/java/br/com/dio/RestSpringSecurityApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestSpringApplicationTests {
+class RestSpringSecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
